ee988f8c6a3bf8c442e5efe778b21c36bc5556f4
Baeknothing <baeknothing@gmail.com>
2021-10-20 21:16:27 +0900

    push

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var stylus = require('stylus');
+const rateLimit = require("express-rate-limit");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,10 +13,16 @@ var animationRouter = require('./routes/animation');
 
 var app = express();
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(limiter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
