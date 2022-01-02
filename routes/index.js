61e167a5a1edef949754e3491ea92578579eac91
BaekNothing <47516849+BaekNothing@users.noreply.github.com>
2022-01-02 14:57:06 +0900

    add async 2021

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,13 @@ var router = express.Router();
 var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   var dirSrc = './public/resource/2021/';
-  try{
-  fs.readdir(dirSrc + 'unity', function(error, unitylist){
-    console.log(unitylist);
-    fs.readdir(dirSrc + 'design', function(error,designlist){
-      console.log(designlist);
-      fs.readdir(dirSrc + 'web', function(error,weblist){
-        console.log(weblist);
-        res.render('2021_index', {
+  var unitylist = await readDir(dirSrc, 'unity');
+  var designlist = await readDir(dirSrc, 'design');
+  var weblist = await readDir(dirSrc, 'web');
+
+  await res.render('2021_index', {
           title: 'Baeknothing_portfolio',
           unitycount: unitylist.length,
           unitylist: unitylist,
@@ -22,10 +19,7 @@ router.get('/', function(req, res, next) {
 
           webcount: weblist.length,
           weblist: weblist
-        });
-      });
-    });
-  });}catch(error){console.log(error);}
+  });
 });
 
 router.get('/2022', async function(req, res, next){
